18845ebc8cfd8372ad6ecae3144717536b6caff7
oikcyx <113072095+oikcyx@users.noreply.github.com>
2023-05-16 19:22:36 -0700

    Spotify Support

--- slash/music/play.js
+++ slash/music/play.js
@@ -4,20 +4,9 @@ const { QueryType } = require("discord-player")
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName("play")
+		.setName("test")
 		.setDescription("play a song")
-        .addSubcommand((subcommand) =>
-			subcommand
-				.setName("song")
-				.setDescription("Play song") 
-				.addStringOption((option) => option.setName("url").setDescription("YT url").setRequired(true)) 
-		)
-        .addSubcommand((subcommand) =>
-			subcommand
-				.setName("playlist")
-				.setDescription("Play playlist") 
-				.addStringOption((option) => option.setName("url").setDescription("YT playlist URL").setRequired(true)) 
-		),
+        .addStringOption((option) => option.setName("url").setDescription("YT url").setRequired(true)),
 
 	run: async ({ client, interaction }) => {
 		if (!interaction.member.voice.channel) return interaction.editReply("Join a call first")
@@ -41,7 +30,8 @@ module.exports = {
 
         let url = interaction.options.getString("url")
         
-        if (interaction.options.getSubcommand() === "playlist") {
+        if (url.startsWith("https://www.youtube.com/playlist?")) {
+            console.log("Loading playlist from Youtube")
             const result = await client.player.search(url, {
                 requestedBy: interaction.user,
                 searchEngine: QueryType.YOUTUBE_PLAYLIST,
@@ -66,8 +56,9 @@ module.exports = {
             embed
                 .setDescription(`**${result.tracks.length} tracks from [${playlist.title}](${playlist.url})** has been added to the queue`) 
                 // .setThumbnail(playlist.thumbnail)
-
-        } else {
+        } 
+        else if (url.startsWith("https://www.youtube.com/watch?")) {
+            console.log("Queueing song from YouTube")
             const result = await client.player.search(url, {
                 requestedBy: interaction.user,
                 searchEngine: QueryType.YOUTUBE_VIDEO,
@@ -88,6 +79,83 @@ module.exports = {
                 .setThumbnail(song.thumbnail)
                 .setFooter({ text: `Duration: ${song.duration}`})
         }
+        else if (url.startsWith("https://open.spotify.com/track/")) {
+            console.log("Queueing song from Spotify")
+            const result = await client.player.search(url, {
+                requestedBy: interaction.user,
+                searchEngine: QueryType.SPOTIFY_SONG,
+            })
+            if (result.tracks.length === 0)
+                return interaction.editReply("No results") 
+            
+            const song = result.tracks[0]
+            
+            if(!queue.connection){
+                await client.player.play(interaction.member.voice.channel, song)
+            }else {
+                queue.addTrack(song);
+            }
+    
+            embed
+                .setDescription(`**[${song.title}](${song.url})** has been added to the queue`) 
+                .setThumbnail(song.thumbnail)
+                .setFooter({ text: `Duration: ${song.duration}`})
+        }
+        else if (url.startsWith("https://open.spotify.com/playlist/")) {
+            console.log("Loading playlist from Spotify")
+            const result = await client.player.search(url, {
+                requestedBy: interaction.user,
+                searchEngine: QueryType.SPOTIFY_PLAYLIST,
+            })
+
+            if (result.tracks.length === 0)
+                return interaction.editReply("no results") 
+            
+            const playlist = result.playlist
+
+            if(!queue.connection){
+                await client.player.play(interaction.member.voice.channel, result.tracks[0])
+                for(let i = 1; i < result.tracks.length; i++ ){
+                    queue.addTrack(result.tracks[i]);
+                }
+            }else {
+                for(let i = 0; i < result.tracks.length; i++ ){
+                    queue.addTrack(result.tracks[i]);
+                }
+            }
+
+            embed
+                .setDescription(`**${result.tracks.length} tracks from [${playlist.title}](${playlist.url})** has been added to the queue`) 
+                .setThumbnail(playlist.thumbnail)
+        } 
+        else if (url.startsWith("https://open.spotify.com/album/")) {
+            console.log("Loading album from Spotify")
+            const result = await client.player.search(url, {
+                requestedBy: interaction.user,
+                searchEngine: QueryType.SPOTIFY_ALBUM,
+            })
+
+            if (result.tracks.length === 0)
+                return interaction.editReply("no results") 
+            
+            const playlist = result.playlist
+
+            if(!queue.connection){
+                await client.player.play(interaction.member.voice.channel, result.tracks[0])
+                for(let i = 1; i < result.tracks.length; i++ ){
+                    queue.addTrack(result.tracks[i]);
+                }
+            }else {
+                for(let i = 0; i < result.tracks.length; i++ ){
+                    queue.addTrack(result.tracks[i]);
+                }
+            }
+
+            embed
+                .setDescription(`**${result.tracks.length} tracks from [${playlist.title}](${playlist.url})** has been added to the queue`) 
+                .setThumbnail(playlist.thumbnail)
+        } 
+
         if (!queue.node.isPlaying) await queue.node.play()
         await interaction.editReply({
             embeds: [embed]
