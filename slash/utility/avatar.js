053c02506e227ba60b6891d285a98dc18e0ba76b
oikcyx <113072095+oikcyx@users.noreply.github.com>
2023-10-07 18:24:32 -0700

    sd

--- slash/utility/avatar.js
+++ slash/utility/avatar.js
@@ -6,7 +6,7 @@ module.exports = {
 		.setName('avatar')
 		.setDescription('Get the avatar URL of the selected user, or your own avatar.')
 		.addUserOption(option => option.setName('target').setDescription('The user\'s avatar to show')),
-	run: async ({ client, interaction }) => {
+	run: async ({interaction }) => {
 		const user = interaction.options.getUser('target');
 		if (user) return interaction.editReply(`${user.username}'s avatar: ${user.displayAvatarURL()}`);
 		return interaction.editReply(`Your avatar: ${interaction.user.displayAvatarURL()}`);
